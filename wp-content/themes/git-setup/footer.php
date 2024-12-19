85b11f57577a9f67bbd5b54d0d92b661f61cc1bd
Gauri Kaushik <gaurikaushik2013@gmail.com>
2024-12-19 10:27:53 +0530

    added custom theme and ACF plugin

---
+++ wp-content/themes/git-setup/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package git-setup
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'git-setup' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'git-setup' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'git-setup' ), 'git-setup', '<a href="https://gotechark.com/">TechArk</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
