f260258d19db0af246c9d06463a2d7204241f461
Gauri Kaushik <gaurikaushik2013@gmail.com>
2024-12-19 13:06:02 +0530

    Update header.php

--- wp-content/themes/git-setup/header.php
+++ wp-content/themes/git-setup/header.php
@@ -23,6 +23,7 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
+	Hello these are the first changes.
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'git-setup' ); ?></a>
 
 	<header id="masthead" class="site-header">
